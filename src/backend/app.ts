d08b42f98cefbb45ca95eac72478a9b9836bd8a8
Valentin Zlydnev <jettary@gmail.com>
2018-06-09 15:12:15 +0300

    Add base config: - typescript - tslint - express

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -1,1 +1,22 @@
-console.log('HELLO, WORLD!')
+import 'reflect-metadata';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as http from 'http';
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000, () => {
+    console.info(`Server Started on ${process.env.PORT || 3000}`);
+});
+
+process.on('uncaughtException', err => {
+    console.error('uncaughtException', err);
+});
+
+process.on('unhandledRejection', err => {
+    console.error('unhandledRejection', err);
+});
