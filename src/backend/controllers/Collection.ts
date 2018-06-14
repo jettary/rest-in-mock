ab15ba8d3cfee919c3c66807df49a0e408e5fbd8
Valentin Zlydnev <jettary@gmail.com>
2018-06-14 11:37:51 +0300

    Add rest of collection controllers

--- src/backend/controllers/Collection.ts
+++ src/backend/controllers/Collection.ts
@@ -3,7 +3,7 @@ import { getRepository, Repository } from 'typeorm';
 import { applyRequest } from '../core/helpers';
 import { Collection } from '../models/Collection';
 import { ControllerInterface } from '../core/Iterfaces';
-import {GeneralException} from "../core/Exceptions";
+import { GeneralException, NotFound } from '../core/Exceptions';
 
 export class CollectionController extends BaseController implements ControllerInterface {
 
@@ -34,10 +34,50 @@ export class CollectionController extends BaseController implements ControllerIn
       return this.res.status(400).json(new GeneralException('INVALID_REQUEST', 'Invalid request'));
     }
 
-    console.log(instance)
     await this.repository.save(instance);
 
-    return this.res.json({ collection: instance });
+    return this.index(instance.id);
+  }
+
+  public async index(id: number) {
+    const collection = await this.repository.findOne({ id });
+
+    if (!collection) {
+      return this.res.status(404).json(new NotFound(`Collection #${id} not found`));
+    }
+
+    return this.res.json({ collection });
+  }
+
+  public async update(id: number) {
+    const collection = await this.repository.findOne({ id });
+
+    if (!collection) {
+      return this.res.status(404).json(new NotFound(`Collection #${id} not found`));
+    }
+
+    Object.assign(collection, this.getModelPayload(Collection));
+
+    const errors = collection.validate();
+    if (errors.length) {
+      return this.res.status(400).json(new GeneralException('INVALID_REQUEST', 'Invalid request'));
+    }
+
+    await this.repository.save(collection);
+
+    return this.index(collection.id);
+  }
+
+  public async remove(id: number) {
+    const collection = await this.repository.findOne({ id });
+
+    if (!collection) {
+      return this.res.status(404).json(new NotFound(`Collection #${id} not found`));
+    }
+
+    await this.repository.remove(collection);
+
+    return this.res.json({ message: 'Job is done'});
   }
 
 }
